dcfd00b61458d94f2793c02c83fa5cab2ff2fd6f
mrdeveloper2007 <mrdeveloper2007@gmail.com>
2024-08-04 00:23:09 +0530

    file name change

--- string manupulation.py
+++ string manupulation.py
@@ -10,4 +10,9 @@ print(a.isalpha())
 print(a.capitalize())
 print(a.upper)
 print(a.split("e"))
+print(a.lstrip("m"))
+print(a.rstrip("per"))
+print(a.startswith("a"))
+print(a.replace("mr","master"))
+
 
