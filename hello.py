1717ea8289b1c00dabe93b9fc8c7735c126e21be
mrdeveloper2007 <mrdeveloper2007@gmail.com>
2024-08-04 00:13:48 +0530

    string manupulation

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
+y=len("helllo")
+print(y)
+print("mrdeveloper".capitalize())
+print("mrdeveloper".count('i'))
+print("mrdeveloper".index('e'))
+a="mrdeveloper"
+print(a.isupper())
+print(a.islower())
+print(a.isalpha())
+print(a.capitalize())
+print(a.upper)
+print(a.split("e"))
 
