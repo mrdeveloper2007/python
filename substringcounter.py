0366abae0dcb49e99108c14b4fe242adc25dfbe8
mrdeveloper2007 <mrdeveloper2007@gmail.com>
2024-08-05 23:08:35 +0530

    practice

--- substringcounter.py
+++ substringcounter.py
@@ -1,4 +1,4 @@
-str= input("enter the string:")
+'''str= input("enter the string:")
 substr = input("enter substr:")
 length=len(str)
 sublen= len(substr)
@@ -15,3 +15,20 @@ while True:
     if start>=length:
         break
 print("no of occurence:",count)
+'''
+str=input("enter string")
+str2=input("enter substring")
+length=len(str)
+length2=len(str2)
+start=0
+count=0
+while True:
+    pos=str.find(str2,start,length)
+    if pos!=-1:
+        count+=1
+        start=pos+length2
+    else:
+        break
+    if start>=length:
+        break
+print(count)
